1d69d06df65335b7f2e3892218cedd13d84c68d2
jdelafon <julien.delafontaine@isb-sib.ch>
2016-07-13 16:29:40 +0200

    My panel; cleanup; start personalizing

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -15,10 +15,9 @@ function Footer() {
   return (
     <footer className="mdl-mini-footer">
       <div className="mdl-mini-footer__left-section">
-        <div className="mdl-logo">© Company Name</div>
+        <div className="mdl-logo">© Vital-IT, 2016</div>
         <ul className="mdl-mini-footer__link-list">
-          <li><Link to="/privacy">Privacy & Terms</Link></li>
-          <li><Link to="/not-found">Not Found (demo)</Link></li>
+          <li><Link to="/">Home</Link></li>
         </ul>
       </div>
       <div className="mdl-mini-footer__right-section">
@@ -37,22 +36,6 @@ function Footer() {
                   17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,
                   16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27
                   14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z"
-                />
-              </svg>
-            </a>
-          </li>
-          <li className="mdl-mini-footer--social-btn" style={{ backgroundColor: 'transparent' }}>
-            <a href="https://twitter.com/ReactStatic" role="button" title="Twitter">
-              <svg width="36" height="36" viewBox="0 0 24 24">
-                <path
-                  fill="#fff" d="M17.71,9.33C18.19,8.93 18.75,8.45 19,7.92C18.59,8.13 18.1,8.26
-                  17.56,8.33C18.06,7.97 18.47,7.5 18.68,6.86C18.16,7.14 17.63,7.38 16.97,
-                  7.5C15.42,5.63 11.71,7.15 12.37,9.95C9.76,9.79 8.17,8.61 6.85,7.16C6.1,8.38
-                  6.75,10.23 7.64,10.74C7.18,10.71 6.83,10.57 6.5,10.41C6.54,11.95 7.39,12.69
-                  8.58,13.09C8.22,13.16 7.82,13.18 7.44,13.12C7.81,14.19 8.58,14.86 9.9,15C9,15.76
-                  7.34,16.29 6,16.08C7.15,16.81 8.46,17.39 10.28,17.31C14.69,17.11 17.64,13.95
-                  17.71,9.33M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1
-                  12,2Z"
                 />
               </svg>
             </a>
--- pages/home/index.js
+++ pages/home/index.js
@@ -9,21 +9,17 @@
  */
 
 import React from 'react';
-import Layout from '../../components/Layout';
+import DotterPanel from '../../components/DotterPanel';
 import { title, html } from './index.md';
 
 class HomePage extends React.Component {
 
-  componentDidMount() {
-    document.title = title;
-  }
 
   render() {
     return (
-      <Layout>
-        <h1>{title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: html }} />
-      </Layout>
+      <div>
+        <DotterPanel />
+        </div>
     );
   }
 
