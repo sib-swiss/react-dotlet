24592047c585fb4792ebd20fcde0e33d00b7b6c2
jdelafon <julien.delafontaine@isb-sib.ch>
2016-07-14 17:08:41 +0200

    trying gitlab ci

--- components/DotterPanel/DotterPanel.js
+++ components/DotterPanel/DotterPanel.js
@@ -49,7 +49,6 @@ class Panel extends React.Component {
         let window_size = this.props.window_size;
         let ws = Math.floor(window_size / 2);   // # of nucleotides on each side
 
-
         var canvas_pt;
         if (L < CANVAS_SIZE) {
             canvas_pt = Math.floor(CANVAS_SIZE / L);
@@ -59,27 +58,27 @@ class Panel extends React.Component {
         let npoints = CANVAS_SIZE / canvas_pt;
         let step = L / npoints;                 // n points -> n-1 steps
 
-        console.log(this.props.window_size, ws, L, step)
+        //console.log(this.props.window_size, ws, L, step)
 
         for (let i=0; i <= npoints; i++) {      // n-1 steps
             let q1 = i * step;                  // position on seq1. First is 0, last is L
             let l1 = Math.max(q1 - ws, 0),
                 r1 = q1 + ws + 1;               // nucleotides window on seq1
-            console.log(s1, [l1,r1], s1.slice(l1, r1))
+            //console.log(s1, [l1,r1], s1.slice(l1, r1))
             let subseq1 = s1.slice(l1, r1);
 
             for (let j=0; j <= i; j++) {        // i steps
                 let q2 = j * step;              // position on seq2
                 let l2 = Math.max(q2 - ws, 0),
-                    r2 = q2 + ws + 1;           // nucleotides window on seq1
+                    r2 = q2 + ws + 1;           // nucleotides window on seq2
                 let subseq2 = s2.slice(l2, r2);
                 let score = DnaScoreMatches(subseq1, subseq2, IDENTITY);
-                console.log([i, j], [q1, q2], [l1,r1], [l2,r2], subseq1, subseq2, score)
+                //console.log([i, j], [q1, q2], [l1,r1], [l2,r2], subseq1, subseq2, score)
                 if (score > 0) {
-                    ctx.globalAlpha=score/this.props.window_size;
-                    ctx.fillRect(i * step * canvas_pt, j * step * canvas_pt, canvas_pt, canvas_pt)
+                    ctx.globalAlpha = score / window_size;
+                    ctx.fillRect(q1 * canvas_pt, q2 * canvas_pt, canvas_pt, canvas_pt)
                     if (i !== j) {
-                        ctx.fillRect(j * step * canvas_pt, i * step * canvas_pt, canvas_pt, canvas_pt)
+                        ctx.fillRect(q2 * canvas_pt, q1 * canvas_pt, canvas_pt, canvas_pt)
                     }
                 }
             }
