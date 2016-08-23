d9a0fa363526af4ad5fb1686c886256b06215195
jdelafon <julien.delafontaine@isb-sib.ch>
2016-08-23 17:01:45 +0200

    Info panel

--- components/InfoPanel/InfoPanel.js
+++ components/InfoPanel/InfoPanel.js
@@ -2,9 +2,7 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import store from '../../core/store';
 import s from './InfoPanel.css';
-//import { SCORING_MATRIX_NAMES } from '../constants/constants';
-//import { SCORING_MATRICES } from '../constants/scoring_matrices/scoring_matrices';
-//import { getSequenceAround } from '../common/helpers';
+import Dotter from '../DotterPanel/dotter';
 
 
 class InfoPanel extends React.Component {
@@ -24,49 +22,30 @@ class InfoPanel extends React.Component {
         });
     }
 
-    calculateMean(scores) {
-        let mean = 0;
-        let n = 0;
-        for (let key of Object.keys(scores)) {
-            let val = scores[key];
-            mean += key * val;
-            n += val;
-        }
-        return n > 0 ? (mean / n).toFixed(2) : 0;
-    }
+    //calculateMean(scores) {
+    //    let mean = 0;
+    //    let n = 0;
+    //    for (let key of Object.keys(scores)) {
+    //        let val = scores[key];
+    //        mean += key * val;
+    //        n += val;
+    //    }
+    //    return n > 0 ? (mean / n).toFixed(2) : 0;
+    //}
 
     render() {
-
-        //let i = this.state.i,
-        //    j = this.state.j,
-        //    s1 = this.state.s1,
-        //    s2 = this.state.s2,
-        //    density = this.state.density,
-        //    windowSize = this.state.windowSize,
-        //    scoringMatrixName = this.state.scoringMatrix;
-
-        //let scoringFunction;
-        //if (scoringMatrixName === SCORING_MATRIX_NAMES.IDENTITY) {
-        //    scoringFunction = calculateMatches;
-        //} else {
-        //    scoringFunction = calculateScore;
-        //}
-        //let matrix = SCORING_MATRICES[scoringMatrixName];
-
-        //let ws = Math.floor(windowSize / 2);
-        //let ss1 = getSequenceAround(s1, i, ws);
-        //let ss2 = getSequenceAround(s2, j, ws);
-        //let localScore = scoringFunction(ss1, ss2, matrix);
-        //let mean = this.calculateMean(density);
+        let state = store.getState();
+        let d = new Dotter(state.canvasSize, state.windowSize, state.s1, state.s2, state.scoringMatrix);
+        let i = state.i, j = state.j;
+        let score = d.scoreAround(i, j);
 
         return (
             <div id="info-panel" className={s.root}>
-                {/*
                 <ul>
-                    <li>{`Score at (${i+1}:${s1[i]}, ${j+1}:${s2[j]}) : ${localScore}`}</li>
-                    <li>{"Mean score: "+ mean}</li>
+                    <li>Sequence 1 length: {d.ls1}</li>
+                    <li>Sequence 2 length: {d.ls2}</li>
+                    <li>{`Score at (${i+1}:${d.s1[i]}, ${j+1}:${d.s2[j]}) : ${score}`}</li>
                 </ul>
-                */}
             </div>
         );
     }
